48740d0a6052c4dc9f8f9d25ccc94c0006f9f503
rizkirisjad <rizkirisjad@gmail.com>
2023-05-28 21:17:41 +0700

    first commit

---
+++ react-fundamentals/src/books.js
@@ -0,0 +1,22 @@
+/** @format */
+
+export const books = [
+  {
+    author: 'Abraham Verghese',
+    title: "The Covenant of Water (Oprah's Book Club)",
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81lxM4A0s2L._AC_UL600_SR600,400_.jpg',
+    id: 1,
+  },
+  {
+    author: 'Wendy Loggia',
+    title: 'Taylor Swift: A Little Golden Book Biography',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81X1plzH9DL._AC_UL600_SR600,400_.jpg',
+    id: 2,
+  },
+  {
+    author: 'Emily Henry',
+    title: 'Happy Place',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71+vQyk44IL._AC_UL600_SR600,400_.jpg',
+    id: 3,
+  },
+];
