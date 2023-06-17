a4201cad7d8b83b52dd862659da32ba521fba797
rizkirisjad <rizkirisjad@gmail.com>
2023-06-17 13:22:08 +0700

    menambah file baru

---
+++ accordion/src/App.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import { useState } from 'react';
+import data from './data';
+import Questions from './Questions';
+
+const App = () => {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <Questions questions={questions} />
+    </main>
+  );
+};
+export default App;
