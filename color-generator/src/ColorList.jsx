71c65aac955a7fb1f755ba8000eea2ddf680b601
rizkirisjad <rizkirisjad@gmail.com>
2023-06-27 19:43:40 +0700

    menambah file project baru

---
+++ color-generator/src/ColorList.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import SingleColor from './SingleColor';
+import { nanoid } from 'nanoid';
+
+const ColorList = ({ colors }) => {
+  return (
+    <section className="colors">
+      {colors.map((color, index) => {
+        return <SingleColor color={color} key={nanoid()} index={index} />;
+      })}
+    </section>
+  );
+};
+export default ColorList;
