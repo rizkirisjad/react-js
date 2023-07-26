08c39e6ebab2de222041df061975d010aa82eec3
rizkirisjad <rizkirisjad@gmail.com>
2023-07-26 20:07:39 +0700

    menambah dark mode

--- unsplash images/src/ThemeToggle.jsx
+++ unsplash images/src/ThemeToggle.jsx
@@ -1,8 +1,20 @@
 /** @format */
 
 import { useGlobalContext } from './context';
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
 
 const ThemeToggle = () => {
-  return <div>ThemeToggle</div>;
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+  return (
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillMoonFill className="toggle-icon" />
+        ) : (
+          <BsFillSunFill lassName="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
 };
 export default ThemeToggle;
