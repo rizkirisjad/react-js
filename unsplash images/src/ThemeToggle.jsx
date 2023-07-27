723d79c2d4ccc7f72330b7f3c0a89ccacd62665c
rizkirisjad <rizkirisjad@gmail.com>
2023-07-27 12:13:26 +0700

    fxed bug di themetoggle

--- unsplash images/src/ThemeToggle.jsx
+++ unsplash images/src/ThemeToggle.jsx
@@ -5,13 +5,14 @@ import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
 
 const ThemeToggle = () => {
   const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+
   return (
     <section className="toggle-container">
       <button className="dark-toggle" onClick={toggleDarkTheme}>
         {isDarkTheme ? (
           <BsFillMoonFill className="toggle-icon" />
         ) : (
-          <BsFillSunFill lassName="toggle-icon" />
+          <BsFillSunFill className="toggle-icon" />
         )}
       </button>
     </section>
