5726825a6489ce045d412715edde520b7147402d
rizkirisjad <rizkirisjad@gmail.com>
2023-08-03 13:18:47 +0700

    menambah logic di projects dan fetchprojects

--- contentful-cms/src/Projects.jsx
+++ contentful-cms/src/Projects.jsx
@@ -1,8 +1,10 @@
 /** @format */
 
-import './fetchProjects';
+import { useFetchProjects } from './fetchProjects';
 
 const Projects = () => {
+  const { loading, projects } = useFetchProjects();
+
   return <div>Projects</div>;
 };
 export default Projects;
