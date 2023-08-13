61e8936950af68313ffe4bdc643fe89b90e06415
rizkirisjad <rizkirisjad@gmail.com>
2023-08-13 15:36:38 +0700

    menambah project baru

---
+++ mix-master/src/App.jsx
@@ -0,0 +1,10 @@
+/** @format */
+
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([{}]);
+
+const App = () => {
+  return <h2>mixmaster starter</h2>;
+};
+export default App;
