709eb80fb467b226ec3e42b9e2e7cbc2388d1d61
rizkirisjad <rizkirisjad@gmail.com>
2023-08-24 14:05:57 +0700

    menambah logic

--- mix-master/src/App.jsx
+++ mix-master/src/App.jsx
@@ -1,6 +1,8 @@
 /** @format */
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import {
   About,
@@ -16,6 +18,14 @@ import { loader as landingLoader } from './pages/Landing';
 import { loader as singleCocktailLoader } from './pages/Cocktail';
 import { action as newsletterAction } from './pages/Newsletter';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
 const router = createBrowserRouter([
   {
     path: '/',
@@ -26,7 +36,7 @@ const router = createBrowserRouter([
         index: true,
         element: <Landing />,
         errorElement: <SinglePageError />,
-        loader: landingLoader,
+        loader: landingLoader(queryClient),
       },
       {
         path: 'cocktail/:id',
@@ -48,6 +58,11 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 export default App;
--- mix-master/src/pages/Landing.jsx
+++ mix-master/src/pages/Landing.jsx
@@ -4,21 +4,35 @@ import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
 import CocktailList from '../components/CocktailList';
 import SearchForm from '../components/SearchForm';
+import { useQuery } from '@tanstack/react-query';
+
+const searchCocktailQuery = (searchTerm) => {
+  return {
+    queryKey: ['search', searchTerm || 'all'],
+    queryFn: async () => {
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return response.data.drinks;
+    },
+  };
+};
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-export const loader = async ({ request }) => {
-  const url = new URL(request.url);
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const url = new URL(request.url);
 
-  const searchTerm = url.searchParams.get('search') || '';
-  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-  return { drinks: response.data.drinks, searchTerm };
-};
+    const searchTerm = url.searchParams.get('search') || '';
+    await queryClient.ensureQueryData(searchCocktailQuery(searchTerm));
+    return { searchTerm };
+  };
 
 const Landing = () => {
-  const { drinks, searchTerm } = useLoaderData();
-  console.log(drinks);
+  const { searchTerm } = useLoaderData();
+  const { data: drinks } = useQuery(searchCocktailQuery(searchTerm));
+
   return (
     <>
       <SearchForm searchTerm={searchTerm} />
