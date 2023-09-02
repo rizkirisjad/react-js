b46df81e4bd6417d4c7c4c4a7377ed9900cf6e3a
rizkirisjad <rizkirisjad@gmail.com>
2023-09-02 19:30:22 +0700

    menambah komponen

--- redux toolkit/src/App.jsx
+++ redux toolkit/src/App.jsx
@@ -1,11 +1,13 @@
 /** @format */
 
+import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <main>
       <Navbar />
+      <CartContainer />
     </main>
   );
 }
