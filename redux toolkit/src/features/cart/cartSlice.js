5a768cba88ececb9c5f2d67ae734a27636d68f14
rizkirisjad <rizkirisjad@gmail.com>
2023-09-04 14:45:47 +0700

    menambah logic cart

--- redux toolkit/src/features/cart/cartSlice.js
+++ redux toolkit/src/features/cart/cartSlice.js
@@ -17,10 +17,24 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, action) => {
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
   },
 });
 
 console.log(cartSlice);
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
