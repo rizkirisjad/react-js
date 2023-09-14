bc92841d285cc01e8fd6ea93bc29b6114857621a
rizkirisjad <rizkirisjad@gmail.com>
2023-09-14 20:03:10 +0700

    menambah utils

---
+++ comfy-store/src/utils/index.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import axios from 'axios';
+
+const productionUrl = 'https://strapi-store-server.onrender.com/api';
+
+export const customFetch = axios.create({
+  baseURL: productionUrl,
+});
