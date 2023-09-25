d32e67a5ba62f0b318ea9a49c84c1b7a147b32cf
rizkirisjad <rizkirisjad@gmail.com>
2023-09-25 14:26:39 +0700

    menambah komponen

--- comfy-store/src/components/Filters.jsx
+++ comfy-store/src/components/Filters.jsx
@@ -1,8 +1,12 @@
 /** @format */
 import { Form, useLoaderData, Link } from 'react-router-dom';
 import FormInput from './FormInput';
+import FormSelect from './FormSelect';
 
 const Filters = () => {
+  const { meta } = useLoaderData();
+  console.log(meta);
+
   return (
     <Form className="bg-base-200 rounded-md px-8 py-8 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/*  SEARCH */}
@@ -11,6 +15,27 @@ const Filters = () => {
         label="search product"
         name="search"
         size="input-sm"
+      />
+      {/*  CATEGORIES */}
+      <FormSelect
+        label="select category"
+        name="category"
+        list={meta.categories}
+        size="select-sm"
+      />
+      {/*  COMPANIES */}
+      <FormSelect
+        label="select company"
+        name="company"
+        list={meta.companies}
+        size="select-sm"
+      />
+      {/*  ORDER  */}
+      <FormSelect
+        label="sort by"
+        name="order"
+        list={['a-z', 'z-a', 'high', 'low']}
+        size="select-sm"
       />
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm">
---
+++ comfy-store/src/components/FormRange.jsx
@@ -0,0 +1,6 @@
+/** @format */
+
+const FormRange = ({ label, name, size }) => {
+  return <div>FormRange</div>;
+};
+export default FormRange;
