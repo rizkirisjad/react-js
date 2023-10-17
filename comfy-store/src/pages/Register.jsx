a59c6825e392560e6bcd061116439a85fee8f273
rizkirisjad <rizkirisjad@gmail.com>
2023-10-17 23:22:28 +0700

    menambah action dan logic register

--- comfy-store/src/pages/Register.jsx
+++ comfy-store/src/pages/Register.jsx
@@ -1,6 +1,25 @@
 /** @format */
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, redirect } from 'react-router-dom';
 import { FormInput, SubmitBtn } from '../components';
+import { customFetch } from '../utils';
+import { toast } from 'react-toastify';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    const response = await customFetch.post('/auth/local/register', data);
+    toast.success('account created successfully');
+    return redirect('/login');
+  } catch (error) {
+    const errorMassage =
+      error?.response?.data?.error?.message ||
+      'please double check your credentials';
+    toast.error(errorMassage);
+    return null;
+  }
+};
 
 const Register = () => {
   return (
@@ -10,9 +29,24 @@ const Register = () => {
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
       >
         <h4 className="text-center text-3xl font-bold">Register</h4>
-        <FormInput label="username" name="username" type="text" />
-        <FormInput label="email" name="email" type="email" />
-        <FormInput label="password" name="password" type="password" />
+        <FormInput
+          label="username"
+          name="username"
+          type="text"
+          defaultValue="conrad f"
+        />
+        <FormInput
+          label="email"
+          name="email"
+          type="email"
+          defaultValue="conradf@gmail.com"
+        />
+        <FormInput
+          label="password"
+          name="password"
+          type="password"
+          defaultValue="secret"
+        />
         <div className="mt-4">
           <SubmitBtn text="Register" />
         </div>
