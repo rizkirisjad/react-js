452748a3c4e9a888c3c6cdc97ae06cec070b06f8
rizkirisjad <rizkirisjad@gmail.com>
2023-10-20 15:55:46 +0700

    menambah logic guest user

--- comfy-store/src/pages/Login.jsx
+++ comfy-store/src/pages/Login.jsx
@@ -27,6 +27,24 @@ export const action =
   };
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const loginAsGuestUser = async () => {
+    try {
+      const response = await customFetch.post('/auth/local', {
+        identifier: 'test@test.com',
+        password: 'secret',
+      });
+      dispatch(loginUser(response.data));
+      toast.success('welcome guest user');
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+      toast.error('guest user error. please try again');
+    }
+  };
+
   return (
     <section className="h-screen grid place-items-center">
       <Form
@@ -34,22 +52,18 @@ const Login = () => {
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
       >
         <h4 className="text-center text-3xl font-bold">Login</h4>
-        <FormInput
-          label="email"
-          name="identifier"
-          type="email"
-          defaultValue="conradf@gmail.com"
-        />
-        <FormInput
-          label="password"
-          name="password"
-          type="password"
-          defaultValue="secret"
-        />
+        <FormInput label="email" name="identifier" type="email" />
+        <FormInput label="password" name="password" type="password" />
         <div className="mt-4">
           <SubmitBtn text="Login" />
         </div>
-        <button className="btn btn-secondary btn-block">guest user</button>
+        <button
+          type="button"
+          className="btn btn-secondary btn-block"
+          onClick={loginAsGuestUser}
+        >
+          guest user
+        </button>
         <p className="text-center">
           not a member yet?{' '}
           <Link
