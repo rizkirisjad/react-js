b9d1510faf709e2f076776e45d7017c1037531c0
rizkirisjad <rizkirisjad@gmail.com>
2023-10-26 16:15:43 +0700

    menambah logic page orders dan orderslist

--- comfy-store/src/pages/Orders.jsx
+++ comfy-store/src/pages/Orders.jsx
@@ -13,11 +13,40 @@ export const loader =
       toast.warn('You must be logged in to view orders');
       return redirect('/login');
     }
-
-    return null;
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+    try {
+      const response = await customFetch.get('/orders', {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      return { orders: response.data.data, meta: response.data.meta };
+    } catch (error) {
+      const errorMassage = console.log(error);
+      error?.response?.data?.error?.message ||
+        'there was an error placing your order';
+      toast.error(errorMassage);
+      if (error.response.status === 401 || 403) {
+        return redirect('/login');
+      }
+      return null;
+    }
   };
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const { meta } = useLoaderData();
+  if (meta.pagination.total < 1) {
+    return <SectionTitle text="please make an order" />;
+  }
+  return (
+    <>
+      <SectionTitle text="Your Orders" />
+      <OrdersList />
+      <PaginationContainer />
+    </>
+  );
 };
 export default Orders;
