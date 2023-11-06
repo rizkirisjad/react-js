87e3b6508a2b59a004b3e4d308b3c624120214bd
rizkirisjad <rizkirisjad@gmail.com>
2023-11-06 16:34:03 +0700

    update landing page

--- jobster/src/pages/Landing.jsx
+++ jobster/src/pages/Landing.jsx
@@ -3,6 +3,27 @@ import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+            distinctio voluptates, sunt porro autem id aliquam minima libero rem
+            repellendus corrupti dolores! Pariatur ad sed placeat, ipsam
+            quibusdam quo dolorum?
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 export default Landing;
