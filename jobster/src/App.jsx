8eea1c5180f2f10525d44f040aa7f77882945ad8
rizkirisjad <rizkirisjad@gmail.com>
2023-12-11 21:25:26 +0700

    add and setup react toastify

--- jobster/src/App.jsx
+++ jobster/src/App.jsx
@@ -2,6 +2,8 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Landing, Error, Dashboard, Register } from './pages';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Route path="register" element={<Register />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <ToastContainer position="top-center" />
     </BrowserRouter>
   );
 }
--- jobster/src/pages/Register.jsx
+++ jobster/src/pages/Register.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import { Logo, FormRow } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
+import { toast } from 'react-toastify';
 
 const initialState = {
   name: '',
@@ -14,11 +15,18 @@ const Register = () => {
   const [values, setValues] = useState(initialState);
 
   const handleChange = (e) => {
-    console.log(e.target);
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(`${name} : ${value}`);
+    setValues({ ...values, [name]: value });
   };
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      toast.error('Please fill out all fields');
+      return;
+    }
   };
 
   const toggleMember = () => {
