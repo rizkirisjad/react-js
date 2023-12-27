2f66340ce9f6e97545b740154a9ef3528c3ca176
rizkirisjad <rizkirisjad@gmail.com>
2023-12-27 13:48:13 +0700

    setup small sidebar

--- jobster/src/App.jsx
+++ jobster/src/App.jsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Landing, Error, Register } from './pages';
+import { Landing, Error, Register, ProtectedRoute } from './pages';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {
@@ -16,7 +16,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Stats />} />
           <Route path="all-jobs" element={<AllJobs />} />
           <Route path="add-jobs" element={<AddJob />} />
--- jobster/src/components/SmallSidebar.jsx
+++ jobster/src/components/SmallSidebar.jsx
@@ -1,10 +1,53 @@
 /** @format */
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleSidebar } from '../features/user/userSlice';
+import links from '../utils/links';
 
 const SmallSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
   return (
     <Wrapper>
-      <h2>SmallSidebar</h2>
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className="content">
+          <button className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, id, icon } = link;
+              return (
+                <NavLink
+                  to={path}
+                  className={({ isActive }) => {
+                    return isActive ? 'nav-link active' : 'nav-link';
+                  }}
+                  key={id}
+                  onClick={toggle}
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
