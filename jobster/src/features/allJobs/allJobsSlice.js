38ecc3720d04247c42a599ae7b721c855ca6a595
rizkirisjad <rizkirisjad@gmail.com>
2024-01-16 22:29:05 +0700

    change page functionality

--- jobster/src/features/allJobs/allJobsSlice.js
+++ jobster/src/features/allJobs/allJobsSlice.js
@@ -67,6 +67,9 @@ const allJobsSlice = createSlice({
     clearFilters: (state) => {
       return { ...state, ...initialFiltersState };
     },
+    changePage: (state, { payload }) => {
+      state.page = payload;
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -98,7 +101,12 @@ const allJobsSlice = createSlice({
   },
 });
 
-export const { showLoading, hideLoading, handleChange, clearFilters } =
-  allJobsSlice.actions;
+export const {
+  showLoading,
+  hideLoading,
+  handleChange,
+  clearFilters,
+  changePage,
+} = allJobsSlice.actions;
 
 export default allJobsSlice.reducer;
