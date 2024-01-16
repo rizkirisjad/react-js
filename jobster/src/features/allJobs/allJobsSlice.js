974115b1cf9f475e2de9411edce770d418605d0b
rizkirisjad <rizkirisjad@gmail.com>
2024-01-16 17:28:17 +0700

    pagination setup

--- jobster/src/features/allJobs/allJobsSlice.js
+++ jobster/src/features/allJobs/allJobsSlice.js
@@ -29,6 +29,7 @@ export const getAllJobs = createAsyncThunk(
     let url = `/jobs`;
     try {
       const resp = await customFetch.get(url);
+      console.log(resp.data);
       return resp.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data.msg);
@@ -41,7 +42,7 @@ export const showStats = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const resp = await customFetch.get('/jobs/stats');
-      console.log(resp.data);
+
       return resp.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data.msg);
@@ -75,6 +76,8 @@ const allJobsSlice = createSlice({
       .addCase(getAllJobs.fulfilled, (state, { payload }) => {
         state.isLoading = false;
         state.jobs = payload.jobs;
+        state.numOfPages = payload.numOfPages;
+        state.totalJobs = payload.totalJobs;
       })
       .addCase(getAllJobs.rejected, (state, { payload }) => {
         state.isLoading = false;
