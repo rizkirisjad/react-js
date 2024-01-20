8dde0dfbd66e0fad9dbf9bee145151515b257c63
rizkirisjad <rizkirisjad@gmail.com>
2024-01-20 13:45:19 +0700

    route error

--- github-search-users/src/App.js
+++ github-search-users/src/App.js
@@ -1,14 +1,24 @@
+/** @format */
+
 import React from 'react';
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="*">
+          <Error></Error>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
