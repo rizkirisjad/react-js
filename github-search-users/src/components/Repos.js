5566b1d8a1b9c73e462e71a44a849fb466272f70
rizkirisjad <rizkirisjad@gmail.com>
2024-01-23 21:38:10 +0700

    chart setup

--- github-search-users/src/components/Repos.js
+++ github-search-users/src/components/Repos.js
@@ -8,10 +8,45 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
 
+  const chartData = [
+    {
+      label: 'Venezuela',
+      value: '290',
+    },
+    {
+      label: 'Saudi',
+      value: '260',
+    },
+    {
+      label: 'Canada',
+      value: '180',
+    },
+    {
+      label: 'Iran',
+      value: '140',
+    },
+    {
+      label: 'Russia',
+      value: '115',
+    },
+    {
+      label: 'UAE',
+      value: '100',
+    },
+    {
+      label: 'US',
+      value: '30',
+    },
+    {
+      label: 'China',
+      value: '30',
+    },
+  ];
+
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <ExampleChart />;
+        <ExampleChart data={chartData} />;
       </Wrapper>
     </section>
   );
