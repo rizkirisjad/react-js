cc64b659e8bc15bc8750ba67b152fe4a2e4771c5
rizkirisjad <rizkirisjad@gmail.com>
2024-01-27 18:36:36 +0700

    column & bar chart

--- github-search-users/src/components/Repos.js
+++ github-search-users/src/components/Repos.js
@@ -77,9 +77,9 @@ const Repos = () => {
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
-        <div></div>
+        <Column3D />
         <Doughnut2D data={mostPopular} />
-        <div></div>
+        <Bar3D />
       </Wrapper>
     </section>
   );
