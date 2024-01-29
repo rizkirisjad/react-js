b14fab5e2df2c0ec9c75b995d3419d6496db968e
rizkirisjad <rizkirisjad@gmail.com>
2024-01-29 13:15:35 +0700

    search component

--- github-search-users/src/components/Search.js
+++ github-search-users/src/components/Search.js
@@ -1,9 +1,37 @@
+/** @format */
+
 import React from 'react';
 import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = React.useState('');
+
+  const handleSubmit = (e) => {
+    console.log(user);
+  };
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onClick={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input
+              type="text"
+              placeholder="enter github user"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            <button type="submit" className="btn">
+              search
+            </button>
+          </div>
+        </form>
+      </Wrapper>
+      ;
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
