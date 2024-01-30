cb5e78c715231d32e6e47a0b25a182214ca4d8a0
rizkirisjad <rizkirisjad@gmail.com>
2024-01-30 20:55:15 +0700

    repos & followers

--- github-search-users/src/context/context.js
+++ github-search-users/src/context/context.js
@@ -27,6 +27,13 @@ const GithubProvider = ({ children }) => {
     );
     if (response) {
       setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+      axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) => {
+        setRepos(response.data);
+      });
+      axios(`${followers_url}?per_page=100`).then((response) => {
+        setFollowers(response.data);
+      });
     } else {
       toggleError(true, 'there is no user with that username');
     }
