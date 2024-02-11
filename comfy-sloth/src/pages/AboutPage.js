c72b922825ebfd9b758892297f6333e106885f8b
rizkirisjad <rizkirisjad@gmail.com>
2024-02-11 17:58:30 +0700

    hero page, about page

--- comfy-sloth/src/pages/AboutPage.js
+++ comfy-sloth/src/pages/AboutPage.js
@@ -1,11 +1,34 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+/** @format */
+
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+            explicabo fugiat quam? Ipsa unde quasi omnis voluptate. Error odio
+            officiis, veritatis sequi aliquam corporis maiores consequatur
+            impedit unde non. Debitis, sint natus! Veritatis voluptas fugiat
+            quaerat labore voluptates culpa, dicta ipsum dolores saepe numquam
+            suscipit sunt quasi. Eum, ipsa aut!
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +56,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
