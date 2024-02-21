08c8a375a8afd17423d591cde65f28ac0f70b74a
rizkirisjad <rizkirisjad@gmail.com>
2024-02-21 16:39:30 +0700

    filter context

--- comfy-sloth/src/index.js
+++ comfy-sloth/src/index.js
@@ -15,6 +15,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <ProductsProvider>
-    <App />
+    <FilterProvider>
+      <App />
+    </FilterProvider>
   </ProductsProvider>
 );
