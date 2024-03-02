53dd709cd758057f6f5a21530ea755efa189bb45
rizkirisjad <rizkirisjad@gmail.com>
2024-03-02 17:28:35 +0700

    cart context setup

--- comfy-sloth/src/index.js
+++ comfy-sloth/src/index.js
@@ -16,7 +16,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ProductsProvider>
     <FilterProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </FilterProvider>
   </ProductsProvider>
 );
